8e347bb7a8b3bb0ecd2e3e0f98dee4b7b7bd3a13
baigugu <baiguguos@gmail.com>
2024-06-24 00:59:46 +0800

    修改设置文件

--- util.py
+++ util.py
@@ -1,9 +1,18 @@
 import json
 import random
 
-#
+# 写入json
 def write_json(data,path):
     pass
-def get_json():
 
+# 读取json
+def get_json():
+    pass
+
+# 随机抽取
+def random_extract():
+    pass
+
+# 随机回复
 def random_reply():
+    pass

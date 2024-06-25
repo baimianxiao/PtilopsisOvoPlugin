4cdb7285b9bb0925efa864404d9c1d878ac1bc33
baigugu <baiguguos@gmail.com>
2024-06-25 17:30:32 +0800

    完善util.py功能

--- interaction.py
+++ interaction.py
@@ -1,5 +1,7 @@
-
+from PtilopsisOvoPlugin.util import random_reply
 
 def poke_reply(plugin_event, Proc):
-    plugin_event.reply("[CQ:image,file=/白面鸮表情包/戳.jpg]")
-    plugin_event.set_block()
+    loginInfo = plugin_event.get_login_info()
+    if(plugin_event.data.target_id == loginInfo["data"]["id"]):
+        plugin_event.reply(random_reply("pokeReply"))
+        plugin_event.set_block()

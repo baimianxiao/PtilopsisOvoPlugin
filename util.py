251ea79e9f827cfd58ed8b63373c18a33a5f2e52
baigugu <baiguguos@gmail.com>
2024-06-25 17:33:49 +0800

    完善util.py功能

--- util.py
+++ util.py
@@ -1,18 +1,43 @@
+# -*- encoding:utf-8 -*-
 import json
 import random
+from os.path import abspath, dirname, join
 
-# 写入json
-def write_json(data,path):
-    pass
+# 取根目录
+dir = dirname(abspath(__file__))
 
-# 读取json
-def get_json():
-    pass
+# 数据路径
+data_dir = join(dir, "..", "..", "data", "PtilopsisOvoPlugin")
+
+# 写json文件
+def write_json(data, path):
+    try:
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+            return True
+    except:
+        return False
+
+
+# 读json文件
+def get_json(path):
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            file = file.read()
+            return json.loads(file)
+    except:
+        return False
 
 # 随机抽取
 def random_extract():
     pass
 
 # 随机回复
-def random_reply():
-    pass
+def random_reply(reply_type):
+    reply_list=get_json(join(data_dir,'data','config.json'))['reply']
+    reply_message = random.choice(reply_list[reply_type])
+    return reply_message
+
+
+
+
